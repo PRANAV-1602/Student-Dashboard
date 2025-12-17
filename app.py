5f67bd1e7d3ab677bfdf493d1b9ea5af98f14ddf
Thota Pranaveshwar <pranaveshwarthota@gmail.com>
2025-12-17 15:19:00 +0530

    Change HTML file reference from fun.html to index.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 from pathlib import Path
 st.set_page_config(page_title="Student Task Dashboard",layout="wide")
-html_file = Path("fun.html").read_text(encoding='utf-8')
+html_file = Path("index.html").read_text(encoding='utf-8')
 st.components.v1.html(html_file, height=800, scrolling=True)
+
